1ef8bf3dd76e50d963489e7312851a2229e636e2
Divyangi <Divyangi@rajputdivyangi.com>
2023-03-14 18:28:40 +0530

    add real estate

---
+++ custom_addons/Real_Estate/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.2',
+    'category': '',
+    'summary': '',
+    'description': """
+This module contains all the common features of Sales Management and eCommerce.
+    """,
+    'depends': [],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+
+    },
+}

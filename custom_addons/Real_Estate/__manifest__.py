d3aec9a0eda4d4ccfabd2ca5203f086fa2f2ed83
Divyangi <Divyangi@rajputdivyangi.com>
2023-03-16 18:37:21 +0530

    add python file

--- custom_addons/Real_Estate/__manifest__.py
+++ custom_addons/Real_Estate/__manifest__.py
@@ -10,7 +10,8 @@
 This module contains all the common features of Sales Management and eCommerce.
     """,
     'depends': [],
-    'data': ['security\ir.model.access.csv'],
+    'data': ['security/ir.model.access.csv',
+             'views/real_estate_views.xml'],
     'demo': [],
     'installable': True,
     'auto_install': False,

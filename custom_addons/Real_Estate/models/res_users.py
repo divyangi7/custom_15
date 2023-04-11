db5c5b2b36838f743994fdbcf6519601453a6836
Divyangi <Divyangi@rajputdivyangi.com>
2023-04-11 20:11:25 +0530

    add custom file

---
+++ custom_addons/Real_Estate/models/res_users.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    property_ids = fields.One2many("real_estate.order", inverse_name="salesperson",
+                                   domain="[('state','=','received')]")
